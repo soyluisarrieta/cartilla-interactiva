67c9d4e33cbff506fa963a3519f9a7de0c2b3b47
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-08 18:13:17 -0500

    Instalar y configurar Body-Parser

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// Middleware: Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Bienvenido a la Cartilla Interactiva')
