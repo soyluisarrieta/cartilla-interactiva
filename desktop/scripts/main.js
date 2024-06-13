b2c5e12394148600cc6369b14d5e5263d28a8fe3
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-12 20:16:08 -0500

    Agregar lógica a los botones de acción

---
+++ desktop/scripts/main.js
@@ -0,0 +1,8 @@
+import { actionButtons } from './actionButtons.js'
+
+// Establecer los botones de acción de ventana
+actionButtons({
+  minimizeId: 'minimize-window',
+  maxResId: 'maximize-window',
+  closeId: 'close-window'
+})
