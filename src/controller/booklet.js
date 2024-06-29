99e7574869d92783fe631df4247c00d251dcb9e0
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-29 16:26:03 -0500

    Añadir método para obtener todos los módulos y renderizarlos en la vista

--- src/controller/booklet.js
+++ src/controller/booklet.js
@@ -1,16 +1,23 @@
 import { NUMBER_BOOKLET_MODULES } from '../constants.js'
+import { BookletModel } from '../models/booklet.js'
 
 export class BookletController {
   /**
+   * Renderizar el página con todos los módulos
+   */
+  renderAllModules = async (req, res) => {
+    const modules = await BookletModel.getAll()
+    res.render('booklet', { modules })
+  }
+
+  /**
    * Renderizar el módulo seleccionado
    */
-  renderModule = (req, res) => {
+  renderModule = async (req, res) => {
     const moduleId = parseInt(req.params.id, 10)
 
-    if (!isNaN(moduleId) && moduleId >= 1 && moduleId <= NUMBER_BOOKLET_MODULES) {
-      res.render('booklet', { moduleId })
-    } else {
-      res.status(404).render('404')
-    }
+    !isNaN(moduleId) && moduleId >= 1 && moduleId <= NUMBER_BOOKLET_MODULES
+      ? res.render('booklet', { moduleId })
+      : res.status(404).render('404')
   }
 }
