ed8e194653d54b2039ec047d79c634700bce302b
Luis Arrieta <luisarrieta796@gmail.com>
2024-07-08 11:45:27 -0500

    Mostrar dinámicamente los slides de cada módulo

--- src/controller/booklet.js
+++ src/controller/booklet.js
@@ -1,4 +1,6 @@
-import { NUMBER_BOOKLET_MODULES } from '../constants.js'
+import path from 'node:path'
+import fs from 'node:fs'
+import { NUMBER_BOOKLET_MODULES, SRC_FOLDER } from '../constants.js'
 import { BookletModel } from '../models/booklet.js'
 
 export class BookletController {
@@ -16,8 +18,27 @@ export class BookletController {
   renderModule = async (req, res) => {
     const moduleId = parseInt(req.params.id, 10)
 
-    !isNaN(moduleId) && moduleId >= 1 && moduleId <= NUMBER_BOOKLET_MODULES
-      ? res.render('page', { moduleId })
-      : res.status(404).render('404')
+    const directoryPath = path.join(SRC_FOLDER, '..', 'browser', 'assets', 'images', 'slides', `module-${moduleId}`)
+
+    if (!isNaN(moduleId) && moduleId >= 1 && moduleId <= NUMBER_BOOKLET_MODULES) {
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+          return res.status(500).send('No se pudo leer el directorio: ' + err)
+        }
+
+        const imageFiles = files.filter(file => {
+          const fileExtension = path.extname(file).toLowerCase()
+          return ['.jpg', '.png'].includes(fileExtension)
+        }).sort((a, b) => {
+          const aNum = parseInt(path.basename(a, path.extname(a)), 10)
+          const bNum = parseInt(path.basename(b, path.extname(b)), 10)
+          return aNum - bNum
+        })
+
+        res.render('slides', { moduleId, imageFiles })
+      })
+    } else {
+      res.status(404).render('404')
+    }
   }
 }
