f274b50927e1d544016fb23fad4484a0cc86c855
Luis Arrieta <luisarrieta796@gmail.com>
2024-07-30 20:34:08 -0500

    Desarrollar el menú de inicio usando una textura para la UI

---
+++ browser/scripts/scenes/LevelSelectionScene.js
@@ -0,0 +1,15 @@
+class LevelSelectionScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'LevelSelectionScene' })
+  }
+
+  preload () {
+    // Cargar los recursos necesarios para la selección de niveles
+  }
+
+  create () {
+    // Mostrar los niveles
+  }
+}
+
+export default LevelSelectionScene
