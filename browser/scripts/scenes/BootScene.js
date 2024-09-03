22873788235538bdc2e6d6b8a894dfb4aa619091
Luis Arrieta <luisarrieta796@gmail.com>
2024-09-03 13:35:57 -0500

    Usar el objeto window como estado global para los ajustes del juego

--- browser/scripts/scenes/BootScene.js
+++ browser/scripts/scenes/BootScene.js
@@ -1,22 +1,52 @@
+import { mergeObjects } from '/scripts/Utils.js'
+
 class BootScene extends Phaser.Scene {
   constructor () {
     super({ key: 'BootScene' })
+
+    // Configuración predeterminada
+    const defaultSettings = {
+      background: 'background',
+      title: {
+        frame: 'bg-title',
+        image: 'uiLvlSelection',
+        imagePositionY: 50,
+        text: 'Selecciona un nivel',
+        fontFamily: 'primaryFont',
+        textPositionY: 65
+      },
+      levels: {
+        name: ['Nivel 1'],
+        image: 'uiLvlSelection',
+        images: [],
+        frame: 'btn-arrow',
+        width: 200,
+        gap: 100,
+        textScale: 0.5,
+        fontFamily: 'primaryFont',
+        buttonScale: 2,
+        positionY: 400
+      },
+      homeButtonY: 230
+    }
+
+    this.settings = mergeObjects(defaultSettings, window.gameSettings)
   }
 
   preload () {
     // Cargar recursos
-    this.load.setPath('/assets/games/')
-    this.load.image('logos', 'resources/logos.png')
-    this.load.image('background', 'game-1/bg-menu.jpg')
+    this.load.setPath('/assets/games')
+    this.load.image('logos', '/resources/logos.png')
+    this.load.image('background', `/${this.settings.key}/${this.settings.background}`)
 
     // Cargar fuentes
-    this.load.setPath('/assets/games/fonts/')
-    this.load.bitmapFont('primaryFont', 'examplefont.png', 'examplefont.fnt')
+    this.load.setPath('/assets/games/fonts')
+    this.load.bitmapFont('primaryFont', '/examplefont.png', '/examplefont.fnt')
 
     // Cargar UI
-    this.load.setPath('/assets/games/ui/')
-    this.load.atlas('uiMainMenu', 'main-menu.png', 'main-menu.json')
-    this.load.atlas('uiLvlSelection', 'level-selection.png', 'level-selection.json')
+    this.load.setPath('/assets/games/ui')
+    this.load.atlas('uiMainMenu', '/main-menu.png', '/main-menu.json')
+    this.load.atlas('uiLvlSelection', '/level-selection.png', '/level-selection.json')
   }
 
   create () {
