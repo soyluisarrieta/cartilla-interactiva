950ee4fd81f97d4cb57e7969d90b30d1b1581321
Luis Arrieta <luisarrieta796@gmail.com>
2024-09-03 11:05:34 -0500

    Añadir LevelSelectionScene y configurar todo el flujo

--- browser/scripts/scenes/GameScene.js
+++ browser/scripts/scenes/GameScene.js
@@ -1,11 +1,46 @@
+import { mergeObjects, addInteractions } from '/scripts/Utils.js'
+
 class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'GameScene' })
+
+    // Configuración predeterminada
+    this.config = {
+      level: 1
+    }
+  }
+
+  init (data) {
+    this.config = mergeObjects(this.config, data)
   }
 
   create () {
+    const { width: widthScreen, height: heightScreen } = this.cameras.main
+
+    // Botón de ir atrás
+    const btnBack = this.add.image(widthScreen / 2, heightScreen - 100, 'uiLvlSelection', 'btn-arrow')
+      .setScale(1.5)
+      .setOrigin(0.5)
+      .setInteractive()
+
+    btnBack.flipX = true
+
+    addInteractions({
+      button: btnBack,
+      key: 'uiLvlSelection',
+      frame: 'btn-arrow',
+      onClick: () => {
+        this.scene.start('LevelSelectionScene', {
+          levels: {
+            name: ['Nivel 1', 'Nivel 3']
+          }
+        })
+      }
+    })
+
     // Crear el contenido del juego aquí
-    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' })
+    this.add.bitmapText(widthScreen / 2, 100, 'primaryFont', `Jugando el juego #${this.config.level}`)
+      .setOrigin(0.5, 0)
   }
 }
 
