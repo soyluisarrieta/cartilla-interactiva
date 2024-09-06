03ce1542bd6b9e57873843fd911530226936ceac
Luis Arrieta <luisarrieta796@gmail.com>
2024-09-06 12:45:56 -0500

    Simplificar la forma en como iniciar el juego

---
+++ browser/scripts/scenes/index.js
@@ -0,0 +1,3 @@
+export { default as BootScene } from './BootScene.js'
+export { default as MenuScene } from './MenuScene.js'
+export { default as LevelSelectionScene } from './LevelSelectionScene.js'
