45463ef679ff6c8dd518348d43afba30d6cc4c43
Luis Arrieta <luisarrieta796@gmail.com>
2024-09-06 12:45:56 -0500

    Simplificar la forma en como iniciar el juego

--- browser/scripts/games/RhythmSlots/index.js
+++ browser/scripts/games/RhythmSlots/index.js
@@ -1,2 +1,15 @@
-export { default as GameScene } from './GameScene.js'
-export { default as InstructionsScene } from './InstructionsScene.js'
+import phaserConfig from '../phaserConfig.js'
+import { BootScene, MenuScene, LevelSelectionScene } from '../../scenes/index.js'
+import InstructionsScene from './InstructionsScene.js'
+import GameScene from './GameScene.js'
+
+export default () => new Phaser.Game({
+  ...phaserConfig,
+  scene: [
+    BootScene,
+    MenuScene,
+    LevelSelectionScene,
+    InstructionsScene,
+    GameScene
+  ]
+})
