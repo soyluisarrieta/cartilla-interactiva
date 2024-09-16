617cf0aff16254bc4730be6ebd17ff121976c89e
Luis Arrieta <luisarrieta796@gmail.com>
2024-09-16 17:51:46 -0500

    Crear objeto con todas las figuras y sus silencios

---
+++ browser/scripts/figures.js
@@ -0,0 +1,50 @@
+export default {
+  // Figures
+  semibreve: {
+    name: 'semibreve',
+    duration: 4,
+    beats: 1
+  },
+  minim: {
+    name: 'minim',
+    duration: 2,
+    beats: 1
+  },
+  crotchet: {
+    name: 'crotchet',
+    duration: 1,
+    beats: 1
+  },
+  quaver: {
+    name: 'quaver',
+    duration: 1,
+    beats: 0.5
+  },
+  semiquaver: {
+    name: 'semiquaver',
+    duration: 1,
+    beats: 0.25
+  },
+
+  // Rest figures
+  semibreveRest: {
+    name: 'semibreve-rest',
+    duration: 4
+  },
+  minimRest: {
+    name: 'minim-rest',
+    duration: 2
+  },
+  crotchetRest: {
+    name: 'crotchet-rest',
+    duration: 1
+  },
+  quaverRest: {
+    name: 'quaver-rest',
+    duration: 1
+  },
+  semiquaverRest: {
+    name: 'semiquaver-rest',
+    duration: 1
+  }
+}
