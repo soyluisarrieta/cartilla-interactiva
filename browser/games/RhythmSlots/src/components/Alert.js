a280d022eb7d4cf10c5568f8bca465070e5c1dfc
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-17 06:34:41 -0500

    Inhabilitar interacción en el juego cuando una alerta está abierta

--- browser/games/RhythmSlots/src/components/Alert.js
+++ browser/games/RhythmSlots/src/components/Alert.js
@@ -14,33 +14,45 @@ export default class Alert {
     buttons = [],
     btnAccept = false
   } = {}) {
-    const alertContainer = this.game.add.container(this.game.scale.width / 2, this.game.scale.height / 2)
+    // Crear un contenedor para toda la alerta
+    const alertContainer = this.game.add.container(0, 0)
+
+    // Crear un fondo de interacción (zone)
+    const overlayZone = this.game.add.zone(0, 0, this.game.scale.width, this.game.scale.height).setOrigin(0).setInteractive()
+    alertContainer.add(overlayZone)
+
+    // Crear el fondo de transparencia
+    const overlayBackground = this.game.add.graphics()
+    overlayBackground.fillStyle(0x000000, 0.5) // Color negro con 0.5 de transparencia
+    overlayBackground.fillRect(0, 0, this.game.scale.width, this.game.scale.height)
+
+    alertContainer.add(overlayBackground)
 
     width += image ? 300 : 0
     height += image ? 300 : 0
 
     // Fondo del modal con borde redondeado
-    const background = this.game.add.graphics()
-    background.fillStyle(this.getColor(type), 1)
-    background.fillRoundedRect(-width / 2, -height / 2, width, height, 20) // 20 es el radio del borde
+    const modalBackground = this.game.add.graphics()
+    modalBackground.fillStyle(this.getColor(type), 1)
+    modalBackground.fillRoundedRect(this.game.scale.width / 2 - width / 2, this.game.scale.height / 2 - height / 2, width, height, 20) // 20 es el radio del borde
 
-    alertContainer.add(background)
+    alertContainer.add(modalBackground)
 
     // Imagen opcional
     if (image) {
-      const modalImage = this.game.add.image(0, -height / 2 + 170, image).setScale(0.5)
+      const modalImage = this.game.add.image(this.game.scale.width / 2, this.game.scale.height / 2 - height / 2 + 170, image).setScale(0.5)
       alertContainer.add(modalImage)
     }
 
     // Título del modal
     if (title) {
-      const modalTitle = this.game.add.bitmapText(0, -height / 2 + (image ? 325 : (message ? 50 : 60)), 'primaryFont', title, 30).setOrigin(0.5, 0)
+      const modalTitle = this.game.add.bitmapText(this.game.scale.width / 2, this.game.scale.height / 2 - height / 2 + (image ? 325 : (message ? 50 : 60)), 'primaryFont', title, 30).setOrigin(0.5, 0)
       alertContainer.add(modalTitle)
     }
 
     // Texto opcional
     if (message) {
-      const modalText = this.game.add.bitmapText(0, -height / 2 + (image ? 375 : 90), 'primaryFont', message, 16).setOrigin(0.5, 0)
+      const modalText = this.game.add.bitmapText(this.game.scale.width / 2, this.game.scale.height / 2 - height / 2 + (image ? 375 : 90), 'primaryFont', message, 16).setOrigin(0.5, 0)
       modalText.setMaxWidth(width - 20)
       alertContainer.add(modalText)
     }
@@ -48,11 +60,11 @@ export default class Alert {
     // Botones opcionales
     const gapX = 100
     const totalButtonWidth = buttons.length * 100 + (buttons.length - 1) * gapX
-    const startX = -totalButtonWidth / 2 + 50
+    const startX = this.game.scale.width / 2 - totalButtonWidth / 2 + 50
 
     buttons.forEach(({ text, onClick }, index) => {
       const buttonX = startX + index * (100 + gapX)
-      const buttonY = height / 2 - 50
+      const buttonY = this.game.scale.height / 2 + height / 2 - 50
 
       const buttonText = this.game.add.text(buttonX, buttonY, text, {
         fontSize: '20px',
@@ -67,7 +79,7 @@ export default class Alert {
 
     // Botón de aceptar
     if (btnAccept) {
-      const buttonAccept = this.game.add.text(0, height / 2 - 30, 'Aceptar', {
+      const buttonAccept = this.game.add.text(this.game.scale.width / 2, this.game.scale.height / 2 + height / 2 - 30, 'Aceptar', {
         fontSize: '20px',
         fill: '#ffffff'
       })
@@ -80,7 +92,7 @@ export default class Alert {
 
     // Botón de cierre si es descartable
     if (dismissible) {
-      const closeButton = this.game.add.text(width / 2 - 30, -height / 2 + 30, '✖', {
+      const closeButton = this.game.add.text(this.game.scale.width / 2 + width / 2 - 30, this.game.scale.height / 2 - height / 2 + 30, '✖', {
         fontSize: '16px',
         color: '#ffffff',
         fontStyle: 'bold'
