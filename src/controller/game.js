55157203e524f9f8c44c8de1ef16ea1ab24fe31e
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-23 18:05:54 -0500

    Añadir lógica al método levelCompleted del controlador de los juegos

--- src/controller/game.js
+++ src/controller/game.js
@@ -1,9 +1,21 @@
+import { GameModel } from '../models/game.js'
+
 export class GameController {
-  constructor (game) {
+  constructor ({ profile, game }) {
+    this.profile = profile
     this.game = game
+    this.gameModel = new GameModel({ profile, game })
   }
 
-  updateLevelData = (data) => {
-    console.log(data, this.game)
+  async levelCompleted (levelData) {
+    try {
+      if (!levelData || typeof levelData !== 'object') {
+        throw new Error('Datos del nivel inválidos.')
+      }
+      await this.gameModel.saveLevel(levelData)
+    } catch (error) {
+      console.error('Error en  el nivel:', error)
+      throw error
+    }
   }
 }
