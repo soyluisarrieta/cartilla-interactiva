cf2a4224d35854bed7b89099160b26a538a04c05
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-23 18:04:55 -0500

    Crear esquema dinámico y método para guardar datos del nivel

---
+++ src/models/game.js
@@ -0,0 +1,30 @@
+import DBLocal from 'db-local'
+
+export class GameModel {
+  constructor ({ profile, game }) {
+    this.profile = profile
+    this.game = game
+    this.path = `db/games/${game.id}`
+
+    const { Schema } = new DBLocal({ path: this.path })
+    this.Game = Schema(profile.id, {
+      _id: { type: String, required: true },
+      level: {
+        name: { type: String, required: true },
+        totalTimer: { type: Number, required: true }
+      },
+      exercises: { type: Array, required: true },
+      timestamp: { type: Number, default: Date.now }
+    })
+  }
+
+  async saveLevel ({ level, exercises }) {
+    try {
+      const data = { level, exercises }
+      const levelCompleted = await this.Game.create(data)
+      await levelCompleted.save()
+    } catch (error) {
+      console.error('Error al guardar el nivel:', error)
+    }
+  }
+}
