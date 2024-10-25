3e303cc01f7ad9b812ab7e6b1f8c9168f0404b35
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-25 15:48:58 -0500

    Crear carpeta core para clases compartidas y AssetLoader para cargar assets en un objeto

---
+++ browser/games/core/AssetLoader.js
@@ -0,0 +1,28 @@
+export default class AssetLoader {
+  constructor (scene) {
+    this.phaser = scene
+  }
+
+  preloadAssets ({ setPath: path, assets }) {
+    path && this.phaser.load.setPath(path)
+
+    assets?.images?.forEach(({ key, path }) => {
+      this.phaser.load.image(key, path)
+    })
+
+    assets?.atlas?.forEach(({ key, dir, fileName }) => {
+      this.phaser.load.atlas(key, `${dir}/${fileName}.png`, `${dir}/${fileName}.json`)
+    })
+
+    assets?.spritesheets?.forEach(({ key, path, frame: { width, height } }) => {
+      this.phaser.load.spritesheet(key, path, {
+        frameWidth: width,
+        frameHeight: height
+      })
+    })
+
+    assets?.audio?.forEach(({ key, path }) => {
+      this.phaser.load.audio(key, path)
+    })
+  }
+}
