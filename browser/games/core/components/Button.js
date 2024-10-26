d87bb2a799c03ae914fc1e6f1407da60d97dfd1c
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-26 12:54:32 -0500

    Corregir el componente Button

--- browser/games/core/components/Button.js
+++ browser/games/core/components/Button.js
@@ -3,48 +3,47 @@ import { addInteractions } from '../../assets/utils/addInteractions.js'
 export default class Button {
   static soundKey = 'soundPress'
 
-  constructor (scene) {
-    this.phaser = scene
-  }
+  static draw (phaser) {
+    return ({
+      key,
+      frame,
+      scene = null,
+      position = [],
+      withInteractions = true,
+      soundKey = Button.soundKey,
+      onClick = null
+    }) => {
+      const { centerX, centerY } = phaser.cameras.main
+      const [x = centerX, y = centerY] = position
+      const button = phaser.add.image(x, y, key, frame)
+        .setOrigin(0.5, 0.5)
+        .setInteractive()
 
-  static draw ({
-    key,
-    frame,
-    scene = null,
-    position: { x, y },
-    withInteractions = true,
-    soundKey = Button.soundKey,
-    onClick = null
-  }) {
-    const button = this.phaser.add.image(x, y, frame, key)
-      .setOrigin(0.5, 0.5)
-      .setInteractive()
+      // Manejar el evento onClick
+      const handleOnClick = () => {
+        phaser.sound.play(soundKey)
+        if (scene) {
+          phaser.scene.start(scene)
+        }
 
-    const handleEvent = () => this.handleOnClick(scene, soundKey, onClick)
+        if (typeof onClick === 'function') {
+          onClick()
+        }
+      }
 
-    if (withInteractions) {
-      addInteractions({
-        button,
-        key: frame,
-        frame: key,
-        onClick: handleEvent()
-      })
-    } else {
-      button.on('pointerup', handleEvent())
-    }
+      // Manejar texturas
+      if (withInteractions) {
+        addInteractions({
+          button,
+          key,
+          frame,
+          onClick: handleOnClick
+        })
+      } else {
+        button.on('pointerup', handleOnClick)
+      }
 
-    return button
-  }
-
-  handleOnClick (scene, soundKey, onClick) {
-    this.phaser.sound.play(soundKey)
-    if (scene) {
-      this.phaser.scene.start(scene)
-    }
-
-    // Manejador del onClick
-    if (typeof onClick === 'function') {
-      onClick()
+      return button
     }
   }
 }
