d15dbfe93c3fe7c79f304ab6cd16a5f4952f8e24
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-26 15:48:14 -0500

    Añadir caracteristica al botón de deshabilitado

--- browser/games/core/components/Button.js
+++ browser/games/core/components/Button.js
@@ -9,6 +9,7 @@ export default class Button {
       frame,
       scene = null,
       position = [],
+      disabled = false,
       withInteractions = true,
       soundKey = Button.soundKey,
       onClick = null
@@ -19,8 +20,11 @@ export default class Button {
         .setOrigin(0.5, 0.5)
         .setInteractive()
 
+      button.disabled = disabled
+
       // Manejar el evento onClick
       const handleOnClick = () => {
+        if (button.disabled) { return }
         phaser.sound.play(soundKey)
         if (scene) {
           phaser.scene.start(scene)
@@ -41,6 +45,15 @@ export default class Button {
         })
       } else {
         button.on('pointerup', handleOnClick)
+      }
+
+      // Método para deshabilitar el botón
+      button.setDisabled = function (disabled = true) {
+        button.disabled = disabled
+        disabled
+          ? this.removeInteractive()
+          : this.setInteractive()
+        this.setAlpha(disabled ? 0.5 : 1)
       }
 
       return button
