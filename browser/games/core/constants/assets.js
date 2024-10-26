031ce761196bf7cef39ceffce4272fb94f9cc2f0
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-26 16:28:19 -0500

    Crear algunas constantes que alojarán los magic strings de los assets

---
+++ browser/games/core/constants/assets.js
@@ -0,0 +1,7 @@
+export const ASSETS = {
+  BUTTONS: {
+    BACK: { key: 'btnBack', frame: 'back-btn' },
+    ARROW_RIGHT: { key: 'uiButtons', frame: 'arrow-right' },
+    ARROW_LEFT: { key: 'uiButtons', frame: 'arrow-left' }
+  }
+}
