999536adf723415e569b027ca8d8aef904cc0a74
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-27 14:38:40 -0500

    Refactorizar en responsabilidades el GameScene del juego 2

--- browser/games/RhythmBlocks/src/scenes/GameScene.js
+++ browser/games/RhythmBlocks/src/scenes/GameScene.js
@@ -7,11 +7,11 @@ import { UI } from '../constants.js'
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: SCENES.GAME })
-
     this.slots = []
     this.blocks = []
   }
 
+  // Inicializa el nivel y perfil del jugador
   init () {
     const selectedLevel = 0
     const profile = getProfile()
@@ -19,6 +19,7 @@ export default class GameScene extends Phaser.Scene {
     this.level = profile.playing.levels[selectedLevel]
   }
 
+  // Crea los elementos principales de la escena
   create () {
     const { width } = this.cameras.main
 
@@ -39,132 +40,160 @@ export default class GameScene extends Phaser.Scene {
     this.drawSlots()
   }
 
-  // Blocks
+  // Crear bloques
   drawBlocks () {
     const totalBlocks = this.level.metrics.blocks
-
     grid({
       totalItems: totalBlocks,
       item: { width: 340, height: 200 },
       maxColumns: 3,
       gap: 40,
       position: [170, 250],
-      element: ({ x, y }) => {
-        const block = this.add
-          .image(x, y, UI.BLOCKS.KEY, UI.BLOCKS.BLOCK(2))
-          .setScale(0.6)
-          .setOrigin(0)
-          .setInteractive({ draggable: true })
+      element: this.setupBlock.bind(this)
+    })
+  }
 
-        // Propiedades iniciales del bloque
-        block.initialX = x
-        block.initialY = y
-        block.currentSlot = null
+  // Configurar bloque
+  setupBlock ({ x, y }) {
+    const block = this.add
+      .image(x, y, UI.BLOCKS.KEY, UI.BLOCKS.BLOCK(2))
+      .setScale(0.6)
+      .setOrigin(0)
+      .setInteractive({ draggable: true })
 
-        // Evento de arrastrar
-        block.on('dragstart', () => {
-          block.setTint(0xff0000)
-          if (block.currentSlot) {
-            // Desocupar slot en el que se encontraba
-            if (block.currentSlot.occupied !== undefined && block.currentSlot.currentBlock !== undefined) {
-              block.currentSlot.occupied = false
-              block.currentSlot.currentBlock = null
-            }
-          }
-        })
+    block.initialX = x
+    block.initialY = y
+    block.currentSlot = null
 
-        // Evento para mover el bloque
-        block.on('drag', (pointer, dragX, dragY) => {
-          block.x = dragX
-          block.y = dragY
-        })
+    this.draggable(block)
+    this.blocks.push(block)
+  }
 
-        // Evento para soltar
-        block.on('dragend', (pointer) => {
-          block.clearTint()
+  // Eventos arrastrar y soltar
+  draggable (block) {
+    block.on('dragstart', () => {
+      block.setTint(0xff0000)
+      if (block.currentSlot) {
+        this.releaseSlot(block.currentSlot)
+      }
+    })
 
-          const droppedInSlot = this.slots.find(slot => {
-            return Phaser.Geom.Rectangle.ContainsPoint(slot.getBounds(), pointer)
-          })
+    block.on('drag', (pointer, dragX, dragY) => {
+      block.x = dragX
+      block.y = dragY
+    })
 
-          // Función para mover el bloque con animación
-          const moveBlock = (block, x, y, onComplete = () => {}) => {
-            block.disableInteractive()
-            this.tweens.add({
-              targets: block,
-              x,
-              y,
-              duration: 500,
-              ease: 'Power2',
-              onComplete: () => {
-                block.setInteractive()
-                onComplete()
-              }
-            })
-          }
+    block.on('dragend', (pointer) => {
+      block.clearTint()
+      const droppedInSlot = this.slots.find(slot => {
+        return Phaser.Geom.Rectangle.ContainsPoint(slot.getBounds(), pointer)
+      })
 
-          if (droppedInSlot) {
-            if (droppedInSlot.occupied) {
-              const occupyingBlock = droppedInSlot.currentBlock
-
-              if (block.currentSlot) {
-                // Intercambiar bloques a sus slots
-                const previousSlot = block.currentSlot
-
-                if (previousSlot.occupied !== undefined && previousSlot.currentBlock !== undefined) {
-                  previousSlot.occupied = false
-                  previousSlot.currentBlock = null
-                }
-
-                block.currentSlot = droppedInSlot
-                occupyingBlock.currentSlot = previousSlot
-
-                previousSlot.currentBlock = occupyingBlock
-                previousSlot.occupied = true
-
-                droppedInSlot.currentBlock = block
-                droppedInSlot.occupied = true
-
-                moveBlock(block, droppedInSlot.x + (droppedInSlot.width * droppedInSlot.scaleX / 2) - (block.width * block.scaleX / 2),
-                  droppedInSlot.y - (droppedInSlot.height * droppedInSlot.scaleY / 2) - (block.height * block.scaleY / 2))
-
-                moveBlock(occupyingBlock, previousSlot.x + (previousSlot.width * previousSlot.scaleX / 2) - (occupyingBlock.width * occupyingBlock.scaleX / 2),
-                  previousSlot.y - (previousSlot.height * previousSlot.scaleY / 2) - (occupyingBlock.height * occupyingBlock.scaleY / 2))
-              } else {
-                // Reemplazar bloque y devolver el otro a su posición inicial
-                moveBlock(occupyingBlock, occupyingBlock.initialX, occupyingBlock.initialY, () => {
-                  occupyingBlock.currentSlot = null
-                  if (occupyingBlock.initialSlot) {
-                    occupyingBlock.initialSlot.occupied = false
-                  }
-                })
-
-                block.currentSlot = droppedInSlot
-                droppedInSlot.currentBlock = block
-                droppedInSlot.occupied = true
-
-                moveBlock(block, droppedInSlot.x + (droppedInSlot.width * droppedInSlot.scaleX / 2) - (block.width * block.scaleX / 2),
-                  droppedInSlot.y - (droppedInSlot.height * droppedInSlot.scaleY / 2) - (block.height * block.scaleY / 2))
-              }
-            } else {
-              // Ubicar bloque en slot vacío
-              moveBlock(block, droppedInSlot.x + (droppedInSlot.width * droppedInSlot.scaleX / 2) - (block.width * block.scaleX / 2),
-                droppedInSlot.y - (droppedInSlot.height * droppedInSlot.scaleX / 2) - (block.height * block.scaleY / 2))
-              droppedInSlot.occupied = true
-              droppedInSlot.currentBlock = block
-              block.currentSlot = droppedInSlot
-            }
-          } else {
-            moveBlock(block, block.initialX, block.initialY)
-          }
-        })
-
-        this.blocks.push(block)
+      if (droppedInSlot) {
+        this.handleBlockDrop(block, droppedInSlot)
+      } else {
+        this.moveBlock(block, block.initialX, block.initialY)
       }
     })
   }
 
-  // Slots
+  // Libera un slot, dejandolo vacío
+  releaseSlot (slot) {
+    if (slot.occupied !== undefined && slot.currentBlock !== undefined) {
+      slot.occupied = false
+      slot.currentBlock = null
+    }
+  }
+
+  // Animación de desplazamiento
+  moveBlock (block, x, y, onComplete = () => {}) {
+    block.disableInteractive()
+    this.tweens.add({
+      targets: block,
+      x,
+      y,
+      duration: 500,
+      ease: 'Power2',
+      onComplete: () => {
+        block.setInteractive()
+        onComplete()
+      }
+    })
+  }
+
+  // Maneja la lógica cuando se suelta un bloque en un slot
+  handleBlockDrop (block, slot) {
+    if (slot.occupied) {
+      const occupyingBlock = slot.currentBlock
+      if (block.currentSlot) {
+        this.swapBlocks(block, occupyingBlock, block.currentSlot, slot)
+      } else {
+        this.replaceBlock(block, occupyingBlock, slot)
+      }
+    } else {
+      this.placeBlockInEmptySlot(block, slot)
+    }
+  }
+
+  // Intercambia dos bloques entre sus respectivos slots
+  swapBlocks (block, occupyingBlock, previousSlot, newSlot) {
+    this.releaseSlot(previousSlot)
+
+    block.currentSlot = newSlot
+    occupyingBlock.currentSlot = previousSlot
+
+    newSlot.currentBlock = block
+    newSlot.occupied = true
+
+    previousSlot.currentBlock = occupyingBlock
+    previousSlot.occupied = true
+
+    this.moveBlock(
+      block,
+      newSlot.x + (newSlot.width * newSlot.scaleX / 2) - (block.width * block.scaleX / 2),
+      newSlot.y - (newSlot.height * newSlot.scaleY / 2) - (block.height * block.scaleY / 2)
+    )
+
+    this.moveBlock(
+      occupyingBlock,
+      previousSlot.x + (previousSlot.width * previousSlot.scaleX / 2) - (occupyingBlock.width * occupyingBlock.scaleX / 2),
+      previousSlot.y - (previousSlot.height * previousSlot.scaleY / 2) - (occupyingBlock.height * occupyingBlock.scaleY / 2)
+    )
+  }
+
+  // Reemplaza un bloque con otro, devolviendo el bloque existente a su posición inicial
+  replaceBlock (block, occupyingBlock, slot) {
+    this.moveBlock(occupyingBlock, occupyingBlock.initialX, occupyingBlock.initialY, () => {
+      occupyingBlock.currentSlot = null
+      if (occupyingBlock.initialSlot) {
+        occupyingBlock.initialSlot.occupied = false
+      }
+    })
+
+    block.currentSlot = slot
+    slot.currentBlock = block
+    slot.occupied = true
+
+    this.moveBlock(
+      block,
+      slot.x + (slot.width * slot.scaleX / 2) - (block.width * block.scaleX / 2),
+      slot.y - (slot.height * slot.scaleY / 2) - (block.height * block.scaleY / 2)
+    )
+  }
+
+  // Coloca un bloque en un slot vacío
+  placeBlockInEmptySlot (block, slot) {
+    this.moveBlock(
+      block,
+      slot.x + (slot.width * slot.scaleX / 2) - (block.width * block.scaleX / 2),
+      slot.y - (slot.height * slot.scaleX / 2) - (block.height * block.scaleY / 2)
+    )
+    slot.occupied = true
+    slot.currentBlock = block
+    block.currentSlot = slot
+  }
+
+  // Crear Slots
   drawSlots () {
     const { height } = this.cameras.main
     const totalSlots = this.level.metrics.slots
@@ -175,18 +204,20 @@ export default class GameScene extends Phaser.Scene {
       maxColumns: 4,
       gap: 40,
       position: [90, height - 50],
-      element: ({ x, y }) => {
-        const slot = this.add
-          .image(x, y, UI.BLOCKS.KEY, UI.BLOCKS.SLOT(2))
-          .setScale(0.7)
-          .setOrigin(0, 1)
-
-        // Propiedades iniciales del slot
-        slot.occupied = false
-        slot.currentBlock = null
-
-        this.slots.push(slot)
-      }
+      element: this.setupSlot.bind(this)
     })
+  }
+
+  // Configurar slot
+  setupSlot ({ x, y }) {
+    const slot = this.add
+      .image(x, y, UI.BLOCKS.KEY, UI.BLOCKS.SLOT(2))
+      .setScale(0.7)
+      .setOrigin(0, 1)
+
+    slot.occupied = false
+    slot.currentBlock = null
+
+    this.slots.push(slot)
   }
 }
