064663328002426c23103682945f2d051a132cd9
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-28 15:50:17 -0500

    Añadir UI y función de reproducir o parar melodia

--- browser/games/RhythmBlocks/src/scenes/GameScene.js
+++ browser/games/RhythmBlocks/src/scenes/GameScene.js
@@ -3,11 +3,13 @@ import Button from '../../../core/components/Button.js'
 import { SCENES, BUTTONS, FONTS } from '../../../core/constants.js'
 import { grid } from '../../../core/utils/grid.js'
 import { Block } from '../components/Block.js'
+import { Melody } from '../components/Melody.js'
 import { Slot } from '../components/Slot.js'
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: SCENES.GAME })
+    this.melody = new Melody(this)
   }
 
   // Inicialización
@@ -24,8 +26,66 @@ export default class GameScene extends Phaser.Scene {
   create () {
     this.backButton()
     this.setTitle('GAME SCENE')
+    this.explanation('Escucha la melodía, arrastra cada parte de la melodía en su casilla correspondiente.')
+    this.melodyButton()
+    this.confirmButton()
     this.drawBlocks()
     this.drawSlots()
+  }
+
+  // Explicación corta
+  explanation (text) {
+    const { width } = this.cameras.main
+    this.add
+      .bitmapText(width - 70, 450, FONTS.SECONDARY, text, 38)
+      .setOrigin(1)
+      .setMaxWidth(500)
+  }
+
+  // Botón: Reproducir melodía
+  melodyButton () {
+    const { tempo } = this.game
+    const { figures, metrics } = this.level
+    const numFigures = metrics.figures * metrics.slots
+    const generatedMelody = this.melody.generate(figures, numFigures)
+
+    const x = this.cameras.main.width - 410
+    const y = 600
+
+    const label = this.add
+      .bitmapText(x, y + 110, FONTS.PRIMARY, 'Melodía', 32)
+      .setOrigin(0.5)
+
+    Button.draw(this)({
+      ...BUTTONS.LIST_MELODY,
+      position: [x, y],
+      withInteractions: false,
+      onClick: async ({ button }) => {
+        if (!this.melody.isPlaying) {
+          label.setText('Parar')
+          button.setTexture(BUTTONS.REPEAT.key, BUTTONS.REPEAT.frame)
+          await this.melody.play(generatedMelody, tempo)
+        }
+        this.melody.stop()
+        label.setText('Melodía')
+        button.setTexture(BUTTONS.LIST_MELODY.key, BUTTONS.LIST_MELODY.frame)
+      }
+    })
+  }
+
+  // Botón: Confirmar melodía
+  confirmButton () {
+    const x = this.cameras.main.width - 210
+    const y = 600
+
+    Button.draw(this)({
+      ...BUTTONS.PLAY,
+      position: [x, y]
+    })
+
+    this.add
+      .bitmapText(x, y + 110, FONTS.PRIMARY, 'Confirmar', 32)
+      .setOrigin(0.5)
   }
 
   // Botón: Ir atrás
@@ -46,8 +106,9 @@ export default class GameScene extends Phaser.Scene {
   }
 
   // Implementar bloques
-  drawBlocks () {
+  async drawBlocks () {
     const { figures, metrics } = this.level
+
     grid({
       totalItems: metrics.blocks,
       item: { width: 340, height: 200 },
