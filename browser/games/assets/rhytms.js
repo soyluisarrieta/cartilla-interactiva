5cd4e0dea269727acf82fcbd329c080bf4cc04cf
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-28 19:11:30 -0500

    Guardar ritmos que se me han ocurrido

---
+++ browser/games/assets/rhytms.js
@@ -0,0 +1,12 @@
+export const RHYTHMS = [
+  [
+    { name: 'crotchet', duration: 1 },
+    { name: 'quaver-duo', duration: 1, beats: 2 },
+    { name: 'crotchet', duration: 1 },
+    { name: 'crotchet', duration: 1 },
+    { name: 'crotchet-rest', duration: 1 },
+    { name: 'crotchet', duration: 1 },
+    { name: 'crotchet', duration: 1 },
+    { name: 'crotchet-rest', duration: 1 }
+  ]
+]
