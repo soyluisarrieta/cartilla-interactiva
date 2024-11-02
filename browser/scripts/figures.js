42f32d57ea05db3ccde8757b9d84d13f604520d3
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-02 15:35:43 -0500

    Añadir propiedad título en cada figura para conocer el nombre en español

--- browser/scripts/figures.js
+++ browser/scripts/figures.js
@@ -2,72 +2,88 @@ export default {
   // Figures
   semibreve: {
     name: 'semibreve',
+    title: 'Redonda',
     duration: 4
   },
   minim: {
     name: 'minim',
+    title: 'Blanca',
     duration: 2
   },
   crotchet: {
     name: 'crotchet',
+    title: 'Begra',
     duration: 1
   },
   quaver: {
     name: 'quaver',
+    title: 'Corchea',
     duration: 0.5
   },
   quaverDuo: {
     name: 'quaver-duo',
     duration: 1,
+    title: 'Corchea Doble',
     beats: 2
   },
   quaverTriplet: {
     name: 'quaver-triplet',
     duration: 1,
+    title: 'Tresillos de Corcheas',
     beats: 3
   },
   semiquaver: {
     name: 'semiquaver',
+    title: 'Semicorchea',
     duration: 0.25
   },
   semiquaverQuad: {
     name: 'semiquaver-quad',
     duration: 1,
+    title: 'Cuatro Semicorcheas',
     beats: 4
   },
 
   // Rest figures
   semibreveRest: {
     name: 'semibreve-rest',
+    title: 'Silencio de Redonda',
     duration: 4
   },
   minimRest: {
     name: 'minim-rest',
+    title: 'Silencio de Blanca',
     duration: 2
   },
   crotchetRest: {
     name: 'crotchet-rest',
+    title: 'Silencio de Negra',
     duration: 1
   },
   quaverRest: {
     name: 'quaver-rest',
+    title: 'Silencio de Corchea',
     duration: 0.5
   },
   semiquaverRest: {
     name: 'semiquaver-rest',
+    title: 'Silencio de Semicorchea',
     duration: 0.25
   },
 
   // Combined figures
   quaverRest_quaver: {
     name: 'quaver-rest_quaver',
+    title: 'Silencio de Corchea y Corchea',
     figures: [
       {
         name: 'quaver-rest',
+        title: 'Silencio de corchea',
         duration: 0.5
       },
       {
         name: 'quaver',
+        title: 'corchea',
         duration: 0.5
       }
     ]
