f283a9203ac7d17ccc24d335e05f9c7086eb2455
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-07 15:51:10 -0500

    Añadir botones para que el usuario decida si el tono aumentó o disminuyó

--- browser/games/ToneTilt/src/scenes/GameScene.js
+++ browser/games/ToneTilt/src/scenes/GameScene.js
@@ -1,5 +1,4 @@
 import Alert from '../../../core/components/Alert.js'
-import Melody from '../../../core/Melody.js'
 import UIManager from '../components/UIManager.js'
 import UIAnimations from '../../../core/UIAnimations.js'
 import { BUTTONS, FONTS, SCENES } from '../../../core/constants.js'
@@ -11,7 +10,6 @@ export default class GameScene extends Phaser.Scene {
 
     this.ui = new UIManager(this)
     this.alert = new Alert(this)
-    this.melody = new Melody(this)
     this.uiAnimations = new UIAnimations(this)
   }
 
@@ -26,6 +24,7 @@ export default class GameScene extends Phaser.Scene {
   create () {
     this.ui.init()
     this.playButton()
+    this.toneDirectionButtons()
     this.start()
 
     // Sonido de inicio de partida
@@ -41,17 +40,14 @@ export default class GameScene extends Phaser.Scene {
   // Botón: Reproducir tonos
   playButton () {
     const { height } = this.cameras.main
-    const [x, y] = [250, height / 2]
-
     const alteration = [1.0, 1.02]
+    const [x, y] = [400, height / 2 + 50]
 
     const button = Button.draw(this)({
       ...BUTTONS.LISTEN_MELODY,
       position: [x, y],
       withInteractions: true,
       onClick: ({ button }) => {
-        console.log(this.tone.setRate, alteration)
-
         // Reproducir el sonido con el tono normal
         this.tone.setRate(alteration[0])
         this.tone.play()
@@ -66,6 +62,41 @@ export default class GameScene extends Phaser.Scene {
 
     const label = this.add
       .bitmapText(x, y + 110, FONTS.PRIMARY, 'Reproducir', 32)
+      .setOrigin(0.5)
+  }
+
+  // Botón: Tono Aumentó/Disminuyó
+  toneDirectionButtons () {
+    const { height } = this.cameras.main
+    const [x, y] = [900, height / 2 + 50]
+    const gap = 200
+
+    // Tono aumentó
+    const buttonIncreased = Button.draw(this)({
+      ...BUTTONS.ARROW_RIGHT,
+      position: [x, y - gap],
+      withInteractions: true,
+      onClick: ({ button }) => {
+        // Verificar si es correcto
+      }
+    })
+
+    const labelIncreased = this.add
+      .bitmapText(x, y - gap + 110, FONTS.PRIMARY, 'Aumentó', 32)
+      .setOrigin(0.5)
+
+    // Tono disminuyó
+    const buttonDecreased = Button.draw(this)({
+      ...BUTTONS.ARROW_LEFT,
+      position: [x, y + gap],
+      withInteractions: true,
+      onClick: ({ button }) => {
+        // Verificar si es correcto
+      }
+    })
+
+    const labelDecreased = this.add
+      .bitmapText(x, y + gap + 110, FONTS.PRIMARY, 'Disminuyó', 32)
       .setOrigin(0.5)
   }
 }
