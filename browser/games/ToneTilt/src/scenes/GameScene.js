9a23d4c26e58fe263bd334746213aed0f2d60362
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-07 15:26:01 -0500

    Añadir botón y que reproduzca dos tonos con diferentes alteraciones

--- browser/games/ToneTilt/src/scenes/GameScene.js
+++ browser/games/ToneTilt/src/scenes/GameScene.js
@@ -2,7 +2,8 @@ import Alert from '../../../core/components/Alert.js'
 import Melody from '../../../core/Melody.js'
 import UIManager from '../components/UIManager.js'
 import UIAnimations from '../../../core/UIAnimations.js'
-import { SCENES } from '../../../core/constants.js'
+import { BUTTONS, FONTS, SCENES } from '../../../core/constants.js'
+import Button from '../../../core/components/Button.js'
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -18,11 +19,13 @@ export default class GameScene extends Phaser.Scene {
   init (level) {
     this.game = window.gameSettings
     this.level = level
+    this.tone = this.sound.add('noteSound')
   }
 
   // Principal
   create () {
     this.ui.init()
+    this.playButton()
     this.start()
 
     // Sonido de inicio de partida
@@ -33,5 +36,36 @@ export default class GameScene extends Phaser.Scene {
   // Iniciar ejercicio
   start () {
 
+  }
+
+  // Botón: Reproducir tonos
+  playButton () {
+    const { height } = this.cameras.main
+    const [x, y] = [250, height / 2]
+
+    const alteration = [1.0, 1.02]
+
+    const button = Button.draw(this)({
+      ...BUTTONS.LISTEN_MELODY,
+      position: [x, y],
+      withInteractions: true,
+      onClick: ({ button }) => {
+        console.log(this.tone.setRate, alteration)
+
+        // Reproducir el sonido con el tono normal
+        this.tone.setRate(alteration[0])
+        this.tone.play()
+
+        // Reproducir el sonido con tono aumentado después de un retraso
+        this.time.delayedCall(this.tone.duration * 1000, () => {
+          this.tone.setRate(alteration[1])
+          this.tone.play()
+        })
+      }
+    })
+
+    const label = this.add
+      .bitmapText(x, y + 110, FONTS.PRIMARY, 'Reproducir', 32)
+      .setOrigin(0.5)
   }
 }
