cdf8445fc22af3a1738a5ac396119b79b93c54ad
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-08 10:49:27 -0500

    Añadir instrucciones de como jugar a ToneTilt

--- browser/games/ToneTilt/assets/how-to-play/intructions.js
+++ browser/games/ToneTilt/assets/how-to-play/intructions.js
@@ -1,11 +1,9 @@
 export const STEPS = [
-  'Paso 1',
-  'Paso 2',
-  'Paso 3',
-  'Paso 4',
-  'Paso 5',
-  'Paso 6',
-  'Paso 7',
-  'Paso 8',
-  'Paso 9'
+  '¡Hola! Bienvenido al juego de tonos, donde podrás poner a prueba tu oído musical.',
+  'Primero, escucha atentamente la secuencia de dos tonos. ¡Presiona el botón de "Reproducir" para comenzar!',
+  'Decide si el segundo tono aumenta o disminuye en comparación con el primero.',
+  'Cuando estés listo para tomar tu decisión, presiona el botón "Aumentó" si crees que el tono sube o "Disminuyó" si crees que baja.',
+  'Por cada respuesta correcta, acumularás puntos y podrás seguir avanzando en el juego.',
+  '¡Ten cuidado! Solo tienes una vida. Si te equivocas, tendrás que empezar de nuevo desde el principio.',
+  '¡Diviértete y reta a tus amigos para ver quién puede acumular más puntos!'
 ]
