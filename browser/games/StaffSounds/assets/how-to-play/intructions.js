f8a1e159cd01dcfd9d0ed5c2e9ebd2c446640025
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-10 23:47:33 -0500

    Añadir instrucciones de como jugar StaffSounds

--- browser/games/StaffSounds/assets/how-to-play/intructions.js
+++ browser/games/StaffSounds/assets/how-to-play/intructions.js
@@ -1,7 +1,20 @@
 export const STEPS = [
-  'Paso 1',
-  'Paso 2',
-  'Paso 3',
-  'Paso 4',
-  'Paso 5'
+  '¡Hola! Bienvenidos. Vamos a divertirnos aprendiendo y practicando el pentagrama musical. ¡Empecemos!',
+
+  // Modo Escribir
+  'En el Modo "Escribir", verás una secuencia de notas en la parte inferior. Debes escribir estas notas en el pentagrama.',
+  'Para añadir una nota al pentagrama, haz clic en la posición correcta y en el orden indicado.',
+  'Cuando estés listo, presiona "Confirmar" para pasar al siguiente ejercicio.',
+
+  // Modo Escuchar
+  'En el Modo "Escuchar", el pentagrama mostrará las primeras tres notas. Debes completar las notas que faltan.',
+  'Escucha la melodía haciendo clic en el botón "Reproducir". Puedes escucharla tantas veces como necesites.',
+  'Cuando termines, presiona "Confirmar" para seguir con el siguiente ejercicio.',
+
+  // Modo Leer
+  'En el Modo "Leer", el pentagrama mostrará una melodía completa.',
+  'Usa los botones de las notas siguiendo el orden que muestra el pentagrama.',
+  'Cuando completes la secuencia, pasarás automáticamente al siguiente ejercicio.',
+
+  'Completa todos los ejercicios para ganar. ¡Buena suerte y disfruta!'
 ]
