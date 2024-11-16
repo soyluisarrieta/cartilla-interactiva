6b42257a3b6fd3f90f9bcfdcac3bf9607ccd42ac
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-16 10:42:27 -0500

    Eliminar configuracion de notas en clave de FA

--- browser/scripts/musicalStaff.js
+++ browser/scripts/musicalStaff.js
@@ -68,64 +68,6 @@ export const MUSICAL_STAFF = [
       DO3s: { name: 'Do#', position: 15, frequency: 138.59 },
       DO3b: { name: 'Dob', position: 15, frequency: 123.47 }
     }
-  },
-
-  // Clave de FA
-  {
-    CLEF: 'BAS#',
-    NOTES: {
-      DO3: { name: 'Do', position: 0, frequency: 130.81 },
-      DO3s: { name: 'Do#', position: 0, frequency: 138.59 },
-      DO3b: { name: 'Dob', position: 0, frequency: 123.47 },
-
-      SI2: { name: 'Si', position: 1, frequency: 123.47 },
-      SI2s: { name: 'Si#', position: 1, frequency: 130.81 },
-      SI2b: { name: 'Sib', position: 1, frequency: 116.54 },
-
-      LA2: { name: 'La', position: 2, frequency: 110.00 },
-      LA2s: { name: 'La#', position: 2, frequency: 116.54 },
-      LA2b: { name: 'Lab', position: 2, frequency: 103.83 },
-
-      SOL2: { name: 'Sol', position: 3, frequency: 98.00 },
-      SOL2s: { name: 'Sol#', position: 3, frequency: 103.83 },
-      SOL2b: { name: 'Solb', position: 3, frequency: 92.50 },
-
-      FA2: { name: 'Fa', position: 4, frequency: 87.31 },
-      FA2s: { name: 'Fa#', position: 4, frequency: 92.50 },
-      FA2b: { name: 'Fab', position: 4, frequency: 82.41 },
-
-      MI2: { name: 'Mi', position: 5, frequency: 82.41 },
-      MI2s: { name: 'Mi#', position: 5, frequency: 87.31 },
-      MI2b: { name: 'Mib', position: 5, frequency: 77.78 },
-
-      RE2: { name: 'Re', position: 6, frequency: 73.42 },
-      RE2s: { name: 'Re#', position: 6, frequency: 77.78 },
-      RE2b: { name: 'Reb', position: 6, frequency: 69.30 },
-
-      DO2: { name: 'Do', position: 7, frequency: 65.41 },
-      DO2s: { name: 'Do#', position: 7, frequency: 69.30 },
-      DO2b: { name: 'Dob', position: 7, frequency: 61.74 },
-
-      SI1: { name: 'Si', position: 8, frequency: 61.74 },
-      SI1s: { name: 'Si#', position: 8, frequency: 65.41 },
-      SI1b: { name: 'Sib', position: 8, frequency: 58.27 },
-
-      LA1: { name: 'La', position: 9, frequency: 55.00 },
-      LA1s: { name: 'La#', position: 9, frequency: 58.27 },
-      LA1b: { name: 'Lab', position: 9, frequency: 51.91 },
-
-      SOL1: { name: 'Sol', position: 10, frequency: 49.00 },
-      SOL1s: { name: 'Sol#', position: 10, frequency: 51.91 },
-      SOL1b: { name: 'Solb', position: 10, frequency: 46.25 },
-
-      FA1: { name: 'Fa', position: 11, frequency: 43.65 },
-      FA1s: { name: 'Fa#', position: 11, frequency: 46.25 },
-      FA1b: { name: 'Fab', position: 11, frequency: 41.20 },
-
-      MI1: { name: 'Mi', position: 12, frequency: 41.20 },
-      MI1s: { name: 'Mi#', position: 12, frequency: 43.65 },
-      MI1b: { name: 'Mib', position: 12, frequency: 38.89 }
-    }
   }
 ]
 
