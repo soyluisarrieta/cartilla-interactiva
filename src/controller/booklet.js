be3f80bdebf24237a20d4e9b9f50ab1d33074a4b
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-16 10:11:38 -0500

    Mover los detalles de los módulos a las constantes y borrar la carpeta bd del proyecto

--- src/controller/booklet.js
+++ src/controller/booklet.js
@@ -1,20 +1,22 @@
-import { BookletModel } from '../models/booklet.js'
+import { MODULES } from '../constants.js'
 
 export class BookletController {
   /**
    * Renderizar la página con todos los módulos
    */
-  renderAllModules = async (req, res) => {
-    const modules = await BookletModel.getAll()
+  renderAllModules = (req, res) => {
+    const modules = MODULES
+    console.log(MODULES)
     res.render('booklet', { modules })
   }
 
   /**
    * Renderizar el módulo seleccionado
    */
-  renderModule = async (req, res) => {
+  renderModule = (req, res) => {
     const moduleId = parseInt(req.params.id, 10)
-    const modules = await BookletModel.getAll()
+    const modules = MODULES
+    console.log(MODULES)
 
     if (!isNaN(moduleId) && moduleId >= 1 && moduleId <= modules.length) {
       res.render(`modules/module-${moduleId}`, { layout: 'slides', moduleId })
