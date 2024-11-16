628fd234760b3dccbd426b961a02021c886a84f3
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-16 13:34:13 -0500

    Terminar de configurar la escena de niveles para grupos de notas en StaffSounds

--- browser/games/StaffSounds/src/scenes/LevelSelectionScene.js
+++ browser/games/StaffSounds/src/scenes/LevelSelectionScene.js
@@ -31,15 +31,22 @@ export default class LevelSelectionScene extends Phaser.Scene {
       .setOrigin(0.5)
 
     // Crear niveles
-    const modes = ['easy', 'medium', 'hard']
+    const modes = this.game.levels.length === 2 ? ['easy', 'medium'] : ['easy', 'medium', 'hard']
     modes.forEach((mode, modeIndex) => {
-      this.add.image(width / 2 + (modeIndex - 1) * 300, 400, 'levels', `level-${mode}`)
+      const offset = this.game.levels.length === 2 ? 150 : 0
+      this.add.image(width / 2 + (modeIndex - 1) * 300 + offset, 400, 'levels', `level-${mode}`)
         .setInteractive()
         .on('pointerup', () => {
           const level = {
             index: modeIndex,
             notes: this.game.notes,
             ...this.game.levels[modeIndex]
+          }
+
+          // Cantidad de notas aleatorias
+          if (this.game?.notes?.groups) {
+            this.showNoteGroupSelection(level, this.game?.notes?.groups)
+            return null
           }
 
           // Escalas musicales
@@ -54,9 +61,67 @@ export default class LevelSelectionScene extends Phaser.Scene {
             return null
           }
 
-          // Notas especificas
+          // Notas específicas
           this.scene.start(SCENES.GAME, level)
         })
+    })
+  }
+
+  // Seleccionar grupo de notas
+  showNoteGroupSelection (level, noteGroups) {
+    const { width, height } = this.cameras.main
+    this.children.removeAll()
+
+    // Imagen de fondo
+    this.add
+      .image(0, 0, 'bgMenu')
+      .setOrigin(0)
+      .setDisplaySize(width, height)
+
+    // Botón: Ir atrás
+    Button.draw(this)({
+      ...BUTTONS.BACK,
+      scene: SCENES.LEVEL_SELECTION,
+      position: [150, 120]
+    })
+
+    // Título
+    this.add
+      .image(width / 2, 120, 'bannerTitle')
+      .setOrigin(0.5)
+
+    this.add
+      .bitmapText(width / 2, 120, FONTS.PRIMARY, 'Selecciona una cantidad')
+      .setOrigin(0.5)
+
+    // Crear botones para cada grupo de notas
+    grid({
+      totalItems: noteGroups.length,
+      maxColumns: 2,
+      item: { width: 500, height: 100 },
+      gap: 100,
+      alignCenter: true,
+      position: [width / 1.8, 600],
+      element: ({ x, y }, i) => {
+        const groupSize = noteGroups[i]
+        const notes = this.game.notes.get(groupSize)
+        const onClick = () => {
+          level.notes = notes
+          this.scene.start(SCENES.GAME, level)
+        }
+
+        Button.draw(this)({
+          ...BUTTONS.ARROW_RIGHT,
+          position: [x, y],
+          onClick
+        }).setScale(0.4)
+
+        this.add
+          .bitmapText(x + 70, y, FONTS.PRIMARY, `${groupSize} notas`, 55)
+          .setOrigin(0, 0.5)
+          .setInteractive()
+          .on('pointerup', onClick)
+      }
     })
   }
 
