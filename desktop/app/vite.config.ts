5f6bd95bf981209db031adcad07beec7d0a2ffd1
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-23 12:09:26 -0500

    Configurar vite para que genere la app fuera del directorio

--- desktop/app/vite.config.ts
+++ desktop/app/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: './',
+  build: {
+    outDir: path.resolve(__dirname, '..', 'dist'),
+    emptyOutDir: true,
+  }
 })
