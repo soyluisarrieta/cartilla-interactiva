9feec3bdc075cfba9f7d0efad3052d2cec4f0077
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-23 16:26:33 -0500

    Eliminar código de la interfaz provicional del panel de control

--- desktop/.prevDesktop/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import { actionButtons } from './actionButtons.js'
-
-// Establecer los botones de acción de ventana
-actionButtons({
-  minimizeId: 'minimize-window',
-  maxResId: 'maximize-window',
-  closeId: 'close-window'
-})
