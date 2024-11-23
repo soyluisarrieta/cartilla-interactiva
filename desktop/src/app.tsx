95e6257734863ab162f8777e35ee12b06ae53854
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-23 15:07:30 -0500

    Integrar componente con los botones de acción funcionando

--- desktop/src/app.tsx
+++ desktop/src/app.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
+import TopBar from "@/components/TopBar";
 
 export function App() {
   return (
     <>
-      <h1 className='text-indigo-300 bg-indigo-800 font-bold text-5xl text-center p-10'>
-        Hola Tailwind
-      </h1>
-      <div className='text-center p-10'>
-        <Button size='lg'>Hola Shadcn</Button>
-      </div>
+      <TopBar />
     </>
   )
 }
---
+++ desktop/src/components/TopBar.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'preact/hooks';
+import { MinusIcon, SquareIcon, XIcon } from 'lucide-react';
+
+const { ipcRenderer } = require('electron');
+
+export default function TopBar() {
+  const [isMaximized, setIsMaximized] = useState(true)
+  
+  // Fn: Cambiar title del botón de maximizar/restaurar
+  const handleMaxiRestore = () => {    
+    ipcRenderer.send('maximizeRestoreApp')
+    ipcRenderer.on('isMaximized', () => { setIsMaximized(true); });
+    ipcRenderer.on('isRestored', () => { setIsMaximized(false); });
+  }
+
+  return (
+    <header 
+      id="top-bar" 
+      className="w-full absolute top-0 left-0 select-none flex justify-end"
+    >
+      <div className='[&>button]:p-2'>
+        <button 
+          id="minimize-window" 
+          title="Minimizar"
+          className='hover:bg-neutral-200'
+          onClick={() => ipcRenderer.send('minimizeApp')}
+        >
+          <MinusIcon size='16' />
+        </button>
+        <button 
+          id="maximize-window" 
+          title={isMaximized ? 'Restaurar' : 'Maximizar'}
+          className='hover:bg-neutral-200'
+          onClick={handleMaxiRestore}
+        >
+          <SquareIcon size='16' />
+        </button>
+        <button 
+          id="close-window" 
+          title="Cerrar"
+          className='hover:bg-neutral-200'
+          onClick={() => ipcRenderer.send('closeApp')}
+        >
+          <XIcon size='16' />
+        </button>
+      </div>
+    </header>
+  );
+}
