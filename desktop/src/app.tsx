faa1153013c8029878a2b5889562bca036285aa4
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-23 17:36:23 -0500

    Renombrar componente filtros y añadir selectores dinamicos

--- desktop/src/app.tsx
+++ desktop/src/app.tsx
@@ -1,4 +1,4 @@
-import Filters from "@/components/Filters";
+import GameSelector from "@/components/GameSelector";
 import Header from "@/components/Header";
 import TopBar from "@/components/TopBar";
 
@@ -7,7 +7,7 @@ export function App() {
     <main className='w-full min-h-screen'>
       <TopBar />
       <Header />
-      <Filters />
+      <GameSelector />
     </main>
   )
 }
---
+++ desktop/src/components/GameSelector.tsx
@@ -0,0 +1,106 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useEffect, useState } from "preact/hooks";
+
+interface GameType {
+  name: string
+  levels: string[]
+  modes?: string[]
+  scales?: string[]
+}
+
+const GAMES: GameType[] = [
+  {
+    name: 'Las Figuras y sus Silencios',
+    levels: ['Fácil', 'Medio', 'Dificil']
+  },
+  {
+    name: 'La Blanca y sus Silencio',
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    name: 'La Redonda y sus Silencio',
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    scales: ['Do mayor', 'Re mayor'],
+    levels: ['Auditivo', 'Visual']  
+  },
+  {
+    name: 'Las Figuras y sus Silencios',
+    levels: ['Fácil', 'Medio', 'Dificil']
+  },
+  {
+    name: 'La Blanca y sus Silencio',
+    levels: ['Fácil', 'Medio', 'Dificil']
+  },
+  {
+    name: 'La Redonda y sus Silencio',
+    levels: ['Fácil', 'Medio', 'Dificil']
+  },
+]
+
+export default function GameSelector() {
+  const [selectedGame, setSelectedGame] = useState(GAMES[0])
+  const [selectedLevel, setSelectedLevel] = useState('0')
+  const [selectedMode, setSelectedMode] = useState('0')
+  const [selectedScale, setSelectedScale] = useState('0')
+
+  return (
+    <div className='flex justify-center gap-3 p-5 flex-wrap'>
+      <Select defaultValue='0' onValueChange={(i) => {
+        setSelectedGame(GAMES[Number(i)]);
+        setSelectedLevel('0');
+        setSelectedMode('0');
+        setSelectedScale('0');
+      }}>
+        <SelectTrigger className="max-w-96 p-5">
+          <SelectValue placeholder="Selecciona un juego" />
+        </SelectTrigger>
+        <SelectContent>
+          {GAMES.map(({ name }, i) => (
+            <SelectItem key={i} value={i.toString()}>
+              {i + 1}. {name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Tabs defaultValue='0' onValueChange={(value) => setSelectedLevel(value)}>
+        <TabsList className='h-auto [&>button]:py-2 [&>button]:px-7'>
+          {selectedGame?.levels.map((level, i) => (
+            <TabsTrigger key={i} value={i.toString()}>
+              {level}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+
+      {selectedGame?.modes && (
+        <Tabs defaultValue='0' onValueChange={(value) => setSelectedMode(value)}>
+          <TabsList className='h-auto [&>button]:py-2 [&>button]:px-7'>
+            {selectedGame.modes.map((mode, i) => (
+              <TabsTrigger key={i} value={i.toString()}>
+                {mode}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      )}
+
+      {selectedGame?.scales && (
+        <Select defaultValue='0' onValueChange={(value) => setSelectedScale(value)}>
+          <SelectTrigger className="w-auto p-5">
+            <SelectValue placeholder="Selecciona una escala" />
+          </SelectTrigger>
+          <SelectContent>
+            {selectedGame.scales.map((scale, i) => (
+              <SelectItem key={i} value={i.toString()}>
+                {scale}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      )}
+    </div>
+  )
+}
