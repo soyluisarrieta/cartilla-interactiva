1982765da8178827fdcb1a74cc7f41fdbd5a5981
Luis Arrieta <luisarrieta796@gmail.com>
2024-11-25 10:35:06 -0500

    Crear componentes para los elementos flotantes

--- desktop/src/app.tsx
+++ desktop/src/app.tsx
@@ -1,22 +1,18 @@
+import Floating from "@/components/Floating";
 import GameSelector from "@/components/GameSelector";
 import Header from "@/components/Header";
 import Leaderboard from "@/components/Leaderboard";
 import TopBar from "@/components/TopBar";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { PLAYERS } from "@/mocks/players";
 
 export function App() {
-  const connectedUsers = PLAYERS.filter(({isOnline}) => isOnline)
-  const isPlural = connectedUsers.length !== 1 && 's'
   return (
     <ScrollArea className="w-full h-screen min-h-screen bg-gradient-to-b from-slate-100 to-white">
       <TopBar />
       <Header />
       <GameSelector />
       <Leaderboard />
-      <span className='fixed bottom-2 right-2 text-lg bg-white/70 filter backdrop-blur-sm py-1 px-5 rounded-full' title='Usuarios conectados'>
-        <span className='font-bold'>{connectedUsers.length}</span> conectado{isPlural}
-      </span>
+      <Floating />
     </ScrollArea>
   )
 }
---
+++ desktop/src/components/Floating.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button"
+import { PLAYERS } from "@/mocks/players"
+import { DatabaseBackupIcon, PowerIcon } from "lucide-react"
+
+export default function Floating() {
+  const connectedUsers = PLAYERS.filter(({isOnline}) => isOnline)
+  const isPlural = connectedUsers.length !== 1 && 's'
+  return (
+    <>
+      <div className='max-w-40 fixed bottom-2 left-2 flex flex-col gap-y-4 p-2 bg-white/70 filter backdrop-blur-sm rounded'>
+        <Button
+          className='scale-125 p-5 bg-white/70 filter backdrop-blur-sm'
+          variant='outline'
+          size='icon'
+          title='Recuperación de perfiles'
+        >
+          <DatabaseBackupIcon className='text-slate-800' strokeWidth={1.5} />
+        </Button>
+        <Button
+          className='scale-125 p-5 bg-white/70 filter backdrop-blur-sm'
+          variant='outline'
+          size='icon'
+          title='Desconectar cartilla'
+        >
+          <PowerIcon className='text-slate-800' strokeWidth={1.9} />
+        </Button>
+      </div>
+
+      <span className='fixed bottom-2 right-2 text-lg bg-white/70 filter backdrop-blur-sm py-1 px-5 rounded-full' title='Usuarios conectados'>
+        <span className='font-bold'>{connectedUsers.length}</span> conectado{isPlural}
+      </span>
+    </>
+  )
+}
