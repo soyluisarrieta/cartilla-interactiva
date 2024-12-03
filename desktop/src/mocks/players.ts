b2a17e82291fd9a52ea59b25ae945196eeda4835
Luis Arrieta <luisarrieta796@gmail.com>
2024-12-02 22:04:30 -0500

    Actualizar mock de jugadores y eliminar mock innecesario

--- desktop/src/mocks/players.ts
+++ desktop/src/mocks/players.ts
@@ -1,10 +1,55 @@
 export const PLAYERS = [
-  {id: 0, name: 'Luis Arrieta', isOnline: false, attempts: 234, avatarId: 1},
-  {id: 1, name: 'Jessica Pistala', isOnline: true, attempts: 643, avatarId: 4},
-  {id: 2, name: 'Sebastián Arristala', isOnline: true, attempts: 873, avatarId: 2},
-  {id: 3, name: 'Martina López', isOnline: false, attempts: 165, avatarId: 5},
-  {id: 4, name: 'Pepito Pérez', isOnline: false, attempts: 254, avatarId: 2},
-  {id: 5, name: 'Julanito detal', isOnline: true, attempts: 28, avatarId: 4},
-  {id: 6, name: 'Paylus Estudio', isOnline: true, attempts: 0, avatarId: 5},
-  {id: 7, name: 'Amazonas Verdes', isOnline: true, attempts: 0, avatarId: 0}
+  {
+      "id": "6747ad42c7163107a44bbe87",
+      "userId": "0f61793e-5b68-400e-9c34-99701101524d",
+      "serial": "0b943fb2-96dd-4f57-8c5d-cf6fe69fbe3c",
+      "username": "Sebas",
+      "isOnline": false,
+      "avatar": "boy2.png",
+      "stats": []
+  },
+  {
+      "id": "6747ad29c7163107a44bbe86",
+      "userId": "27a5c290-d79f-4395-9aa2-81cd57143b50",
+      "serial": "0b943fb2-96dd-4f57-8c5d-cf6fe69fbe3c",
+      "username": "Luis",
+      "isOnline": true,
+      "avatar": "boy1.png",
+      "stats": []
+  },
+  {
+      "id": "6747ad0dc7163107a44bbe85",
+      "userId": "f22656e3-8d5a-4f05-b6fc-a227b4bc6cf7",
+      "serial": "0b943fb2-96dd-4f57-8c5d-cf6fe69fbe3c",
+      "username": "J",
+      "isOnline": true,
+      "avatar": "girl1.png",
+      "stats": [
+          {
+              "id": "6747a52417fee4089cb30cbc",
+              "levelName": "unique",
+              "score": 800,
+              "time": 22,
+              "timestamp": 1732748580273
+          }
+      ]
+  },
+  {
+      "id": "6747acf7c7163107a44bbe84",
+      "userId": "059afb8a-d790-466b-b8b6-7298f3e7f641",
+      "serial": "0b943fb2-96dd-4f57-8c5d-cf6fe69fbe3c",
+      "username": "L",
+      "isOnline": false,
+      "avatar": "boy1.png",
+      "stats": []
+  },
+  {
+      "id": "6747ae75c8535e43fc13c8fd",
+      "userId": "4cf2eb61-77b7-4002-8603-5ebdd210b4be",
+      "serial": "c52cad3a-63e2-4880-9569-1ac0d0486930",
+      "username": "A",
+      "isOnline": true,
+      "avatar": "girl2.png",
+      "stats": []
+  }
 ]
