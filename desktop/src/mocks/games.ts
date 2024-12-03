fc92183d2bbc09255f2c63194ca97276b0fa4835
Luis Arrieta <luisarrieta796@gmail.com>
2024-12-02 22:02:39 -0500

    Añadir propiedades provicionales de los juegos a las constantes desktop

--- desktop/src/mocks/games.ts
+++ desktop/src/mocks/games.ts
@@ -1,29 +1,108 @@
+export const DICT_LEVELS =  {
+  easy: 'Fácil',
+  medium: 'Intermedio',
+  hard: 'Dificil',
+}
+
 export const GAMES = [
   {
+    id: 'g1-the-figures-and-their-silences',
     name: 'Las Figuras y sus Silencios',
-    levels: ['Fácil', 'Medio', 'Dificil']
+    levels: ['easy', 'medium', 'hard']
   },
   {
-    name: 'La Blanca y sus Silencio',
+    id: 'g2-what-rhythm-is-playing',
+    name: 'Que ritmo suena',
+    levels: ['easy', 'hard']
+  },
+  {
+    id: 'g3-the-eighth-note-figure',
+    name: 'La figura corchea',
+    levels: ['easy', 'hard']
+  },
+  {
+    id: 'g4-the-sixteenth-note-figure',
+    name: 'La figura semicorchea',
+    // 🚩 Falta actualizar
     modes: ['Escribir', 'Escuchar', 'Leer'],
     levels: ['Auditivo', 'Visual']
   },
   {
-    name: 'La Redonda y sus Silencio',
+    id: 'g5-the-eighth-rest-figure',
+    name: 'La figura silencio de corchea',
+    // 🚩 Falta actualizar
     modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g6-play-with-the-rhythm',
+    name: 'Juega con el ritmo',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g7-do-the-tones-go-up-or-down',
+    name: 'Suben o bajan los tonos',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g8-do-re-mi',
+    name: 'Do, Re y Mi',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g9-fa-sol-la',
+    name: 'Fa, Sol y La',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g10-la-si-do',
+    name: 'La, Si y do',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g11-do-scale',
+    name: 'Escala de Do',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g12-treble-clef',
+    name: 'Clave de Sol',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g13-major-scales',
+    name: 'Escalas mayores',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual']
+  },
+  {
+    id: 'g14-chromatic-scales',
+    name: 'Escalas cromaticas',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual'],
+  },
+  {
+    id: 'g15-major-and-minor-scales',
+    name: 'Escalas mayores y menores',
+    // 🚩 Falta actualizar
+    modes: ['Escribir', 'Escuchar', 'Leer'],
+    levels: ['Auditivo', 'Visual'],
     scales: ['Do mayor', 'Re mayor'],
-    levels: ['Auditivo', 'Visual']  
-  },
-  {
-    name: 'Las Figuras y sus Silencios',
-    levels: ['Fácil', 'Medio', 'Dificil']
-  },
-  {
-    name: 'La Blanca y sus Silencio',
-    levels: ['Fácil', 'Medio', 'Dificil']
-  },
-  {
-    name: 'La Redonda y sus Silencio',
-    levels: ['Fácil', 'Medio', 'Dificil']
   },
 ]
