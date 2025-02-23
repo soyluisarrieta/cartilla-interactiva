843eef338b3883d1581e0dc2f8a363aad86e0b6b
Luis Arrieta <luisarrieta796@gmail.com>
2025-02-23 14:49:28 -0500

    Implementar texturas a las alertas y ajustar botones

--- browser/games/core/components/Alert.js
+++ browser/games/core/components/Alert.js
@@ -1,14 +1,21 @@
 import UIAnimations from '../UIAnimations.js'
-import { FONTS } from '../constants.js'
+import { ALERTS, FONTS } from '../constants.js'
 
 export default class Alert {
+  static textures = {
+    gameover: ALERTS.GAMEOVER,
+    success: ALERTS.SUCCESS,
+    completed: ALERTS.COMPLETED,
+    error: ALERTS.FAILED
+  }
+
   constructor (scene) {
     this.scene = scene
     this.uiAnimations = new UIAnimations(scene)
   }
 
   showAlert (title, {
-    type = 'default',
+    type = 'error',
     duration = 0,
     width = 400,
     height = 200,
@@ -20,6 +27,9 @@ export default class Alert {
   } = {}) {
     width += image ? 300 : 0
     height += image ? 300 : 0
+
+    console.log(type)
+    const { BG, ICON } = Alert.textures[type]
 
     // Crear un contenedor para toda la alerta centrada
     const alertContainer = this.scene.add
@@ -41,29 +51,26 @@ export default class Alert {
 
     alertContainer.add(overlayBackground)
 
-    // Fondo del modal con borde redondeado
-    const modalBackground = this.scene.add.graphics()
-    modalBackground.fillStyle(this.getColor(type), 1)
-    modalBackground.fillRoundedRect(-width / 2, -height / 2, width, height, 20)
-
+    // Imagen de fondo del modal
+    const modalBackground = this.scene.add
+      .image(0, 0, ALERTS.KEY, BG)
+      .setDisplaySize(width, height)
     alertContainer.add(modalBackground)
 
     // Imagen opcional
-    if (image) {
-      const modalImage = this.scene.add.image(0, -height / 2 + 170, image).setScale(0.5)
-      alertContainer.add(modalImage)
-    }
+    const modalImage = this.scene.add
+      .image(0, -height / 2 + 110, ALERTS.KEY, ICON)
+      .setScale(0.6)
+    alertContainer.add(modalImage)
 
     // Título del modal
-    if (title) {
-      const modalTitle = this.scene.add.bitmapText(0, -height / 2 + (image ? 325 : (message ? 50 : 60)), FONTS.PRIMARY, title, 40)
-        .setOrigin(0.5, 0)
-      alertContainer.add(modalTitle)
-    }
+    const modalTitle = this.scene.add.bitmapText(0, -height / 2 + 235, FONTS.PRIMARY, title, 60)
+      .setOrigin(0.5, 0)
+    alertContainer.add(modalTitle)
 
     // Texto opcional
     if (message) {
-      const modalText = this.scene.add.bitmapText(0, -height / 2 + (image ? 380 : 100), FONTS.SECONDARY, message, 24)
+      const modalText = this.scene.add.bitmapText(0, -height / 2 + 325, FONTS.SECONDARY, message, 24)
         .setOrigin(0.5, 0)
         .setMaxWidth(width - 50)
         .setCenterAlign()
@@ -71,7 +78,7 @@ export default class Alert {
     }
 
     // Botones opcionales
-    const gapX = 100
+    const gapX = 150
     const totalButtonWidth = buttons.length * 100 + (buttons.length - 1) * gapX
     const startX = -totalButtonWidth / 2 + 50
 
@@ -79,40 +86,69 @@ export default class Alert {
       const buttonX = startX + index * (100 + gapX)
       const buttonY = height / 2 - 50
 
-      const buttonText = this.scene.add.text(buttonX, buttonY, text, {
-        fontSize: '20px',
-        fill: '#ffffff'
+      const buttonBackground = this.scene.add.graphics()
+      buttonBackground.fillStyle(0xffffff, 1)
+      buttonBackground.fillRoundedRect(buttonX - 120, buttonY - 40, 240, 50, 10)
+
+      // Hacer el fondo del botón interactivo
+      buttonBackground.setInteractive(new Phaser.Geom.Rectangle(buttonX - 120, buttonY - 40, 240, 50), Phaser.Geom.Rectangle.Contains)
+
+      const buttonText = this.scene.add.text(buttonX, buttonY - 15, text, {
+        fontSize: '24px',
+        fill: '#0f0f0f'
       })
         .setOrigin(0.5)
         .setInteractive()
+
+      // Asignar el mismo evento pointerup al fondo del botón
+      buttonBackground.on('pointerup', () => {
+        onClick && onClick()
+        this.dismissAlert(alertContainer)
+      })
 
       buttonText.on('pointerup', () => {
         onClick && onClick()
         this.dismissAlert(alertContainer)
       })
+
+      alertContainer.add(buttonBackground)
       alertContainer.add(buttonText)
     })
 
     // Botón de aceptar
     if (btnAccept) {
-      const buttonAccept = this.scene.add.text(0, height / 2 - 30, 'Aceptar', {
-        fontSize: '20px',
-        fill: '#ffffff'
-      })
+      const buttonAcceptBackground = this.scene.add.graphics()
+      buttonAcceptBackground.fillStyle(0xffffff, 1)
+      buttonAcceptBackground.fillRoundedRect(-100, height / 2 - 90, 200, 50, 10)
+
+      // Hacer el fondo del botón de aceptar interactivo
+      buttonAcceptBackground.setInteractive(new Phaser.Geom.Rectangle(-100, height / 2 - 90, 200, 50), Phaser.Geom.Rectangle.Contains)
+
+      const buttonAccept = this.scene.add
+        .text(0, height / 2 - 65, 'Aceptar', {
+          fontSize: '24px',
+          fill: '#0f0f0f'
+        })
         .setOrigin(0.5)
         .setInteractive()
 
+      // Asignar el mismo evento pointerup al fondo del botón de aceptar
+      buttonAcceptBackground.on('pointerup', () => this.dismissAlert(alertContainer))
+
       buttonAccept.on('pointerup', () => this.dismissAlert(alertContainer))
+
+      alertContainer.add(buttonAcceptBackground)
       alertContainer.add(buttonAccept)
     }
 
     // Botón de cierre si es descartable
     if (dismissible) {
-      const closeButton = this.scene.add.text(width / 2 - 30, -height / 2 + 30, '✖', {
-        fontSize: '16px',
-        color: '#ffffff',
-        fontStyle: 'bold'
-      }).setOrigin(0.5)
+      const closeButton = this.scene.add
+        .text(width / 2 - 30, -height / 2 + 30, '✖', {
+          fontSize: '16px',
+          color: '#ffffff',
+          fontStyle: 'bold'
+        }).setOrigin(0.5)
 
       closeButton.setInteractive()
       closeButton.on('pointerup', () => this.dismissAlert(alertContainer))
@@ -136,16 +172,6 @@ export default class Alert {
         callback: () => this.dismissAlert(alertContainer),
         callbackScope: this
       })
-    }
-  }
-
-  getColor (type) {
-    switch (type) {
-      case 'success': return 0x4CAF50
-      case 'error': return 0xF44336
-      case 'warning': return 0xff9800
-      case 'info': return 0x2196F3
-      default: return 0x111111
     }
   }
 
